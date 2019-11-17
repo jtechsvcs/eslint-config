df3c388231d47f16191da02915baab7e21d5d8c1
Jeremy Albright <myrlin1@gmail.com>
2019-11-16 23:08:04 -1000

    formatting

--- configs/overrides/config/overrides.js
+++ configs/overrides/config/overrides.js
@@ -1,36 +1,36 @@
 const standard = [];
 const typescript = [
-	{
-		"files": ["*.ts", "*.tsx"],
-		"rules": {
-			"indent": "off",
-		},
-	},
-	{
-		"files": ["*.js", "*.vue"],
-		"rules": {
-			"@typescript-eslint/indent": "off",
-			"@typescript-eslint/no-var-requires": "off",
-		},
-	},
+    {
+        "files": ["*.ts", "*.tsx"],
+        "rules": {
+            "indent": "off",
+        },
+    },
+    {
+        "files": ["*.js", "*.vue"],
+        "rules": {
+            "@typescript-eslint/indent": "off",
+            "@typescript-eslint/no-var-requires": "off",
+        },
+    },
 ];
 const vue = [
-	{
-		"files": ["*.vue"],
-		"rules": {
-			"indent": "off",
-		},
-	},
+    {
+        "files": ["*.vue"],
+        "rules": {
+            "indent": "off",
+        },
+    },
 ];
 const react = [];
 const vueTypescript = vue.concat(typescript);
 const reactTypescript = react.concat(typescript);
 
 module.exports = {
-	standard,
-	typescript,
-	vue,
-	react,
-	vueTypescript,
-	reactTypescript,
+    standard,
+    typescript,
+    vue,
+    react,
+    vueTypescript,
+    reactTypescript,
 };
