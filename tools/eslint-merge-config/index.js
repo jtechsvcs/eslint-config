226ee1a83a9b805783bc9c1adee7e1624f0fdfa9
Jeremy Albright <myrlin1@gmail.com>
2020-04-05 18:04:33 -1000

    do recursive merge

--- tools/eslint-merge-config/index.js
+++ tools/eslint-merge-config/index.js
@@ -1,26 +1,38 @@
 /**
- * Does a shallow merge of object <from> to object <to>.
- * Traverses each of the keys in Object <from>, and, if
- * they are an array, concatenates them to <to>.  If 
- * they are an object, then it will extend <to>.
+ * Does a recursive merge of object `from` to object `to`.
+ * Traverses each of the keys in Object `from`, allowing for:
+ * 
+ * * If the value of a key is an array, it will be concatenated
+ * 	 onto `to`.
+ * * If the value of a key is an object, it will recurse into the object,
+ * 	 following these same procedures, and extend `to` with the results.
  * @param {Object} from Object to copy from
  * @param {Object} to Object to copy to
  * @returns {Object} The object that has been extended
  */
 function merge(from, to) {
-	for (var key in from) {
-		let hasKey = to.hasOwnProperty(key);
-		if (Array.isArray(from[key])) {
-			if (!hasKey) to[key] = [];
-			to[key] = to[key].concat(from[key]);
-		} else if (typeof from[key] === "object") {
-			if (!hasKey) to[key] = {};
-			Object.assign(to[key], from[key]);
-		} else {
-			to[key] = from[key];
-		}
-	}
-	return to;
+    const mergedInto = Object.assign({}, to);
+    for (var key in from) {
+        let hasKey = mergedInto.hasOwnProperty(key);
+        if (Array.isArray(from[key])) {
+            if (
+                !hasKey || 
+				!(mergedInto[key] instanceof Array)
+            ) mergedInto[key] = [];
+
+            mergedInto[key].push(...from[key]);
+        } else if (typeof from[key] === "object") {
+            if (
+                !hasKey || 
+				!(typeof mergedInto[key] === "object")
+            ) mergedInto[key] = {};
+			
+            Object.assign(mergedInto[key], merge(from[key], mergedInto[key]));
+        } else {
+            mergedInto[key] = from[key];
+        }
+    }
+    return mergedInto;
 }
 
 module.exports = merge;
